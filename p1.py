951a434d8f883c7df5cb2aa0bbeea148c4e52d69
Mikhail Motorov <mnmotorov@yandex.ru>
2023-09-17 15:53:59 +0300

    unittest 2

--- p1.py
+++ p1.py
@@ -4,7 +4,7 @@ import pack.ListDir as ld
 import pack.WriteJsonFile as jf
 import unittest
 
-result = list()
+
 
 def work(file: str) -> None:
     """Главная функция. Управляет функциями listDirectories() и writeToJSON
@@ -14,19 +14,27 @@ def work(file: str) -> None:
     AttributeError: 'list' object has no attribute 'json'
     >>> work("result.json")
     """
-    file = os.getcwd() + "\\" + file
-    result = ld.listDirectories("i:\\DOS")
+    result = list()
+    file = os.getcwd() + "/" + file
+    result = ld.listDirectories("i:/DOS")
+    print(file)
     jf.writeToJSON(result, file)
 
 class TestWork(unittest.TestCase):
     def test_one_work(self):
-        self.assertEqual(work(result.json), None)
+        self.assertEqual(work("result"), None)
     
     def test_two_work(self):
         self.assertEqual(work("result.json"), None)
+    
+    def test_three_work(self):
+        self.assertEqual(work("rr"), None)
+
+    def test_four_work(self):
+        self.assertEqual(work("pp.json"), None)
 
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
 
--- pack/WriteJsonFile.py
+++ pack/WriteJsonFile.py
@@ -1,5 +1,4 @@
 import json
-
 def writeToJSON(ls: list, file: str) -> None:
     """Функция сохранеет результат работы функции listDirectories в json файл"""
     with open(file, "w") as f:
